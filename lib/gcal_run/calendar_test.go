173642466d0d05bef70fe6362a8da8dc0f2cb1f8
fetaro <tetsutaro.watanabe@goinc.jp>
2025-01-19 23:39:40 +0900

    自動テスト修正

--- lib/gcal_run/calendar_test.go
+++ lib/gcal_run/calendar_test.go
@@ -1,19 +1,31 @@
 package gcal_run
 
 import (
+	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// このテストは/tmp/gcal_run_test/credential.jsonと/tmp/gcal_run_test/oauth_tokenが存在する場合のみ実行される
 func TestGCalGetCalendarEvents(t *testing.T) {
+	credentialPath := "/tmp/gcal_run_test/credential.json"
+	// credentialPathが存在するか確認する
+	_, err := os.Stat(credentialPath)
+	if err != nil {
+		t.Skip("クレデンシャルファイルが存在しないので、テストはスキップ")
+	}
+	// oauth_tokenが存在するか確認する
+	oauthTokenPath := "/tmp/oauth_token"
+	_, err = os.Stat(oauthTokenPath)
+	if err != nil {
+		t.Skip("oauth_tokenファイルが存在しないので、テストはスキップ")
+	}
 	dateStr := "2023-12-30T11:15:00+09:00"
 	// Time.time型に変換する
 	basisTime, err := time.Parse(time.RFC3339, dateStr)
 	assert.NoError(t, err)
-	gcal := NewCalendar("/tmp/gcal_run_test/credential.json", "/tmp/oauth_token")
+	gcal := NewCalendar(credentialPath, oauthTokenPath)
 	calendarEventList, err := gcal.GetCalendarEvents(basisTime)
 	assert.NoError(t, err)
 	assert.Equal(t, "test1", calendarEventList.Items[0].Summary)
